2f4ddd30e6103e983c2f3e48e01e0353bd05e14e
liao2000 <liao2000@livemail.tw>
2020-07-28 11:28:30 +0800

    loop

---
+++ basic/05-loop.dart
@@ -0,0 +1,21 @@
+void main(){
+    var list = [1, 2, 3];
+    for(var i in list){
+        print(i);
+    }
+
+    //A label can be used with break and continue to control the flow more precisely.
+    outloop:
+    for(var i =0; i < 10; i++){
+        inloop:
+        for(var j=0; j < 9; j++){
+            if(j == 2){
+                break inloop;
+            }
+            if(i == 5){
+                break outloop;
+            }
+            print("i:${i} j:${j}");
+        }
+    }
+}
