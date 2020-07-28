efe4ee7251228dff2e11d14cdfbadfe74e873065
liao2000 <liao2000@livemail.tw>
2020-07-28 11:28:17 +0800

    decision

---
+++ basic/04-decision.dart
@@ -0,0 +1,17 @@
+void main(){
+    int num = 10;
+    switch(num){
+        case 1: {
+            print(1);
+        }
+        break;
+        case 2: {
+            print(2);
+        }
+        break;
+        default:{
+            print("other");
+        }
+        break;
+    }
+}
