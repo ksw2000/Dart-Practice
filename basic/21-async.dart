d23d2363ffd38d4043b6e8366571cee22d8e4988
liao2000 <liao2000@livemail.tw>
2020-08-01 23:13:49 +0800

    async

---
+++ basic/21-async.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+import 'dart:async';
+
+void main(){
+    File file = new File('../file-example/file.txt');
+
+    Future<String> f = file.readAsString();
+
+    // Asynchronous method
+    f.then((data) => print(data));
+
+    // once file is read, call back method is invoked
+    print("End of main");
+    // this get printed first, showing fileReading is non blocking or async
+}
