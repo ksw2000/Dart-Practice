a5edfd73921b342d8e7e6e0a1c3ee232402b1440
liao2000 <liao2000@livemail.tw>
2021-04-11 13:06:33 +0800

    Modify comments

--- basic/12-class.dart
+++ basic/12-class.dart
@@ -3,7 +3,7 @@
 *   <fields>
 *   <getters/setters>
 *   <constructors>
-*   <fucntions>
+*   <functions>
 * }
 * */
 
@@ -31,20 +31,14 @@ class Rectangle{
 // However the default ones can be overridden by explicitly defining a setter/getter
 
 class Student{
-    String name;
-    int age;
+    // fields
+    String name = '';
+    int age = 0;
 
     // setter
     void set stud_name(String name){
         this.name = name;
     }
-
-    // getter
-    String get stud_name{
-        return name;
-    }
-
-    Student({this.name: "", this.age: 0});
 
     void set stud_age(int age){
         if(age <= 0){
@@ -54,9 +48,26 @@ class Student{
         }
     }
 
-    int get stud_age{
-        return age;
-    }
+    // getter
+    String get stud_name => name;
+    /*
+    * same as follow:
+    * String get stud_name{
+    *   return name;
+    * }
+    * */
+    int get stud_age => age;
+
+    // constructors
+    Student({this.name = "", this.age = 0});
+
+    /*
+    * same as follow:
+    * Student({name, age}){
+    *   this.name = name ?? "";
+    *   this.age  = age ?? 0;
+    * }
+    * */
 }
 
 void main(){
@@ -67,8 +78,8 @@ void main(){
     print("r1's area = ${r1.area}");
 
     Student s1 = Student();
-    s1.stud_name = '我妻由乃';    // setter
-    s1.stud_age = 14;                // setter
-    print(s1.stud_name);            // getter
-    print(s1.stud_age);             // getter
+    s1.stud_name = '大道寺知世';       // setter
+    s1.stud_age = 13;                // setter
+    print(s1.stud_name);             // getter
+    print(s1.stud_age);              // getter
 }
