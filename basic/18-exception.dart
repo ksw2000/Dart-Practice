c3859f8dadeda55aabf269dc1c108a31dc718c4d
Kashiwa <liao2000@livemail.tw>
2024-04-05 03:35:39 +0800

    'IntegerDivisionByZeroException' is deprecated

--- basic/18-exception.dart
+++ basic/18-exception.dart
@@ -1,43 +1,45 @@
-void test_age(int age){
-    if(age < 0){
-        throw FormatException();
-    }
+void test_age(int age) {
+  if (age < 0) {
+    throw FormatException();
+  }
 }
 
 // custom exceptions
-class AmtException implements Exception{
-    String errMsg() => 'Amount should be greater than zero';
+class AmtException implements Exception {
+  String errMsg() => 'Amount should be greater than zero';
 }
 
-void withdraw_amt(int amt){
-    if(amt <= 0){
-        throw AmtException();
-    }
+void withdraw_amt(int amt) {
+  if (amt <= 0) {
+    throw AmtException();
+  }
 }
 
-void main(){
-    int x = 12;
-    int y = 0;
-    int res;
+void main() {
+  int x = 12;
+  int y = 0;
+  int res;
 
-    // /  : return double
-    // ~/ : return int
+  // /  : return double
+  // ~/ : return int
 
-    // way1
-    try{
-        res = x ~/ y;
-    }on IntegerDivisionByZeroException{
-        print('Cannot divide by zero');
-    }
+  // way1
+  try {
+    res = x ~/ y;
+  } on UnsupportedError {
+    // 'IntegerDivisionByZeroException' is deprecated and shouldn't
+    // be used. Use UnsupportedError instead.
+    print('Cannot divide by zero');
+  }
 
-    // way2
-    try{
-        res = x ~/ y;
-    }catch(e){
-        print(e);
-    }
+  // way2
+  try {
+    res = x ~/ y;
+  } catch (e) {
+    print(e);
+  }
 
-    /*
+  /*
         try{
 
         } on Exception1 {
@@ -49,15 +51,15 @@ void main(){
         }
     */
 
-    try{
-        test_age(-1);
-    }catch(e){
-        print(e);
-    }
+  try {
+    test_age(-1);
+  } catch (e) {
+    print(e);
+  }
 
-    try{
-        withdraw_amt(-1);
-    }catch(e){
-        print(e);
-    }
+  try {
+    withdraw_amt(-1);
+  } catch (e) {
+    print(e);
+  }
 }
