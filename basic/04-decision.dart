5e50fc0f8a83e527ae15e1ab052089125cb4180c
ksw2000 <13825170+ksw2000@users.noreply.github.com>
2024-09-04 20:08:19 +0800

    Update 04-decision.dart

--- basic/04-decision.dart
+++ basic/04-decision.dart
@@ -1,25 +1,36 @@
 void main() {
-  int num = 10;
+  int num = 5;
   switch (num) {
     case 1:
-      {
-        print(1);
-      }
-      break;
+      print(1);
     case 2:
-      {
-        print(2);
-      }
-      break;
+      print(2);
     default:
-      {
-        print("other");
-      }
-      break;
+      print("other");
+  }
+
+  switch (num) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+      print("1~4");
+    case 5:
+    case 6:
+    case 7:
+    case 8:
+      print("5~8");
+    default:
+      print("other");
   }
 
   // Conditional expressions
-  bool isEven;
-  isEven = num & 1 == 1 ? false : true;
-  print("$num is even? $isEven");
+  print("$num is ${num >= 0 ? "non-negative" : "negative"}");
+
+  var n = 15;
+  if (n % 2 == 1) {
+    print("$n is odd");
+  } else {
+    print("$n is even");
+  }
 }
