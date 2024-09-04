8f2afd7405f11d832abeccaa97cd01b41d650d9b
ksw2000 <13825170+ksw2000@users.noreply.github.com>
2024-09-04 20:09:26 +0800

    Update 05-loop.dart

--- basic/05-loop.dart
+++ basic/05-loop.dart
@@ -4,18 +4,33 @@ void main() {
     print(i);
   }
 
+  for (int i = 0; i < 5; i++) {
+    print(i);
+  }
+
   //A label can be used with break and continue to control the flow more precisely.
-  outloop:
+  l1:
   for (var i = 0; i < 10; i++) {
-    inloop:
+    l2:
     for (var j = 0; j < 9; j++) {
       if (j == 2) {
-        break inloop;
+        break l2;
       }
       if (i == 5) {
-        break outloop;
+        break l1;
       }
-      print("i:${i} j:${j}");
+      print("i:$i j:$j");
     }
   }
+
+  int i = 0;
+  while (i < 5) {
+    print(i);
+    i++;
+  }
+
+  do {
+    print(i);
+    i++;
+  } while (i < 10);
 }
